0df0d6e57dc6883c4fa8b9e74976d49905229723
vyskoc <vyskoc@email.cz>
2021-04-12 22:05:38 +0200

    upraven příklad 34

--- 7/priklad34.py
+++ 7/priklad34.py
@@ -4,15 +4,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-teplota = pd.read_csv("temperature.csv")
-#vyber = teplota[(teplota["City"] == "Helsinki") | (teplota["City"] == "Miami Beach")| (teplota["City"] == "Tokyo")]
-helsinky = teplota[teplota["City"] == "Helsinki"].loc[:,"AvgTemperature"]
-miami = teplota[teplota["City"] == "Miami Beach"].loc[:,"AvgTemperature"]
-tokyo = teplota[teplota["City"] == "Tokyo"].loc[:,"AvgTemperature"]
-print(helsinky, miami, tokyo)
-teploty = helsinky.to_frame(name="Helsinky")
-teploty["Miami Beach"] = miami
-teploty["Tokyo"] = tokyo
-print(teploty)
+teplota = pd.read_csv("temperature.csv", index_col=False)
+helsinky = teplota[teplota["City"] == "Helsinki"].loc[:,["Day","AvgTemperature"]]
+miami = teplota[teplota["City"] == "Miami Beach"].loc[:,["Day","AvgTemperature"]]
+tokyo = teplota[teplota["City"] == "Tokyo"].loc[:,["Day","AvgTemperature"]]
+helsinky = helsinky.set_index('Day') # SPOJIT JAKO TABULKU????
+miami = miami.set_index('Day')
+tokyo = tokyo.set_index('Day')
+teploty=pd.merge(helsinky, miami, on="Day")
+teploty=pd.merge(teploty, tokyo, on="Day")
+teploty.rename(columns={"AvgTemperature_x":"Helsinky",
+                        "AvgTemperature_y":"Miami",
+                        "AvgTemperature":"Tokyo" }, inplace=True)
 teploty.plot(kind = "box")
-plt.show()
+plt.show() #Helsinky nejchaldnější, Miami nejteplejší
