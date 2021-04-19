04ab776ef7c98b45d341c6eba54eb80dd2faf01e
vyskoc <vyskoc@email.cz>
2021-04-19 19:42:33 +0200

    Příklady 9. týden

--- 8/pujcovna/katalog/models.py
+++ 8/pujcovna/katalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+    rz = models.CharField(max_length=7)
+    znacka = models.CharField(max_length=100)
+    km = models.IntegerField()
+    tk = models.DateField()
+
+class Zakaznik(models.Model):
+    jmeno = models.CharField(max_length=25)
+    prijmeni = models.CharField(max_length=100)
+    ridicsky_prukaz = models.CharField(max_length=15)
+    datum_narozeni = models.DateField()
+
+class Vypujcka(models.Model):
+    vypujceni = models.DateTimeField()
+    vraceni = models.DateTimeField()
+    cena = models.IntegerField()
